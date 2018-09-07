aec3695568e93cbbca2d096727ac553dd02d4bbc
qfjkl <30939879+qfjkl@users.noreply.github.com>
2018-09-07 19:03:05 +0200

    squelette de la classe catsHasMedias

---
+++ libs/cat_has_media.php
@@ -0,0 +1,32 @@
+<?php
+
+class catsHasMedias{
+
+  
+  public function __construct(){
+
+  }
+
+/* 
+@function  Name: createCats
+@params string: nom de la catégorie et la description de la catégorie 
+@return boolean 
+*/     
+  public function catsHasMediasAdd(){
+    
+    
+  } 
+  public function catsHasMediasRead(){
+    
+    
+  }
+  public function catsHasMediasUpdate(){
+    
+    
+  }
+  public function catsHasMediasDelete(){
+    
+    
+  }
+  
+}
