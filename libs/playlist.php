4acdff96e6a968ad5042d2d45a569f77c5147371
qfjkl <30939879+qfjkl@users.noreply.github.com>
2018-09-07 19:08:12 +0200

    squelette de la classe playlist

---
+++ libs/playlist.php
@@ -0,0 +1,32 @@
+<?php
+
+class playlist{
+
+  
+  public function __construct(){
+
+  }
+
+/* 
+@function  Name: createCats
+@params string: nom de la catégorie et la description de la catégorie 
+@return boolean 
+*/     
+  public function addPlaylist(){
+    
+    
+  } 
+  public function readPlaylist(){
+    
+    
+  }
+  public function updatePlaylist(){
+    
+    
+  }
+  public function deletePlaylist(){
+    
+    
+  }
+  
+}
