a6d54476c2b60425492b33615cdcc5d9201da17a
qfjkl <30939879+qfjkl@users.noreply.github.com>
2018-09-07 19:13:03 +0200

    squelette de la classe admin

---
+++ libs/admin.php
@@ -0,0 +1,28 @@
+<?php
+
+class admin{
+
+  
+  public function __construct(){
+
+  }
+
+    
+  public function addAdmin(){
+    
+    
+  } 
+  public function readAdmin(){
+    
+    
+  }
+  public function updateAdmin(){
+    
+    
+  }
+  public function deleteAdmin(){
+    
+    
+  }
+  
+}
