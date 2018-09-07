c0ba069777cc48459e905a460332fce33d56a848
qfjkl <30939879+qfjkl@users.noreply.github.com>
2018-09-07 19:16:52 +0200

    squelette de la classe playlistHasMedias

---
+++ libs/playlists_has_medias.php
@@ -0,0 +1,28 @@
+<?php
+
+class playlistHasMedias{
+
+  
+  public function __construct(){
+
+  }
+
+    
+  public function addplaylistHasMedias(){
+    
+    
+  } 
+  public function readplaylistHasMedias(){
+    
+    
+  }
+  public function updateplaylistHasMedias(){
+    
+    
+  }
+  public function deleteplaylistHasMedias(){
+    
+    
+  }
+  
+}
